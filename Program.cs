4481e2622e2d48ddf2e480e8ca7d508d715823c2
ehsan1767 <120755613+ehsan1767@users.noreply.github.com>
2023-03-01 14:43:10 +0330

    add project chain of

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ChainOfResponsibility
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type an int to divide Max Int By");
+            try
+            {
+                LineReader.ReadLine();
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("quit trying to divide By zero, you might break something");
+            }
+
+        }
+    }
+}
---
+++ TheGreatDivider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    public static class TheGreatDivider
+    {
+        public static void MaxIntDividedBy(string number)
+        {
+            if (string.IsNullOrEmpty(number))
+                throw new ArgumentException("Nothing Entered","number");
+            Console.WriteLine(int.MaxValue/int.Parse(number));
+        }
+    }
+}
